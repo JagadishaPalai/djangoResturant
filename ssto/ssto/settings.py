479db3b0a641f1624399cd8dbbf7ee58a1e3c9c5
JagadishaPalai <palaijagadisha2001@gmail.com>
2024-12-22 19:18:56 +0530

    push

--- ssto/ssto/settings.py
+++ ssto/ssto/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-5rpc3$!o&268b!v07v3ng_g)@6un8hpdoony%7-@8%#=^og-h+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
