a288de9c69e7d6533beb4fdfa71764346408028d
JagadishaPalai <palaijagadisha2001@gmail.com>
2024-12-22 15:56:20 +0530

    push

--- ssto/asto/urls.py
+++ ssto/asto/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
     path('contact/', views.contact_view, name='contact'),
     path('thank-you/', views.thank_you_view, name='thank_you'),    
 ]
